0e7fb9743ebb5828d97dc13ddb9e8e2516338fa5
Gaurav Thakur <gaurav2.thakur@ril.com>
2022-11-10 15:40:53 +0530

    update the banner again!

--- packages/Banner/src/index.tsx
+++ packages/Banner/src/index.tsx
@@ -4,7 +4,7 @@ interface BannerProps {
 export function Banner({ text }: BannerProps) {
     return (
         <div>
-            <h1>This is a banner</h1>
+            <h1>This is a banner.</h1>
             <h1>{text}</h1>
         </div>
     );
